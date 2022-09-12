e32cfbc6947061b27e5eaa1bb5a27c016654e780
huangsh <942212756@qq.com>
2022-09-12 22:58:17 +0800

    feat:修改时间戳接口

--- main.py
+++ main.py
@@ -197,9 +197,9 @@ def main(_user, _passwd, min_1, max_1):
 
 # 获取时间戳
 def get_time():
-    url = 'http://api.m.taobao.com/rest/api3.do?api=mtop.common.getTimestamp'
+    url = 'https://api.m.jd.com/client.action?functionId=queryMaterialProducts&client=wh5'
     response = requests.get(url, headers=headers).json()
-    t = response['data']['t']
+    t = response['currentTime2']
     return t
 
 
