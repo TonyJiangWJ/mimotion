c36c84a58b5763805b7d4dae5a0ec86e12faf4dc
huangsh <942212756@qq.com>
2023-03-05 17:26:54 +0800

    feat:增加邮箱登录

--- main.py
+++ main.py
@@ -111,7 +111,13 @@ def get_code(location):
 
 # 登录
 def login(user, password):
-    url1 = "https://api-user.huami.com/registrations/+86" + user + "/tokens"
+    is_phone = False
+    if re.match(r'\d{11}', user):
+        is_phone = True
+    if is_phone:
+        url1 = "https://api-user.huami.com/registrations/+86" + user + "/tokens"
+    else:
+        url1 = "https://api-user.huami.com/registrations/" + user + "/tokens"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
         "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2"
@@ -132,16 +138,33 @@ def login(user, password):
     # print(code)
 
     url2 = "https://account.huami.com/v2/client/login"
-    data2 = {
-        "app_name": "com.xiaomi.hm.health",
-        "app_version": "4.6.0",
-        "code": f"{code}",
-        "country_code": "CN",
-        "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
-        "device_model": "phone",
-        "grant_type": "access_token",
-        "third_name": "huami_phone",
-    }
+    if is_phone:
+        data2 = {
+            "app_name": "com.xiaomi.hm.health",
+            "app_version": "4.6.0",
+            "code": f"{code}",
+            "country_code": "CN",
+            "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
+            "device_model": "phone",
+            "grant_type": "access_token",
+            "third_name": "huami_phone",
+        }
+    else:
+        data2 = {
+            "allow_registration=": "false",
+            "app_name": "com.xiaomi.hm.health",
+            "app_version": "6.3.5",
+            "code": f"{code}",
+            "country_code": "CN",
+            "device_id": "2C8B4939-0CCD-4E94-8CBA-CB8EA6E613A1",
+            "device_model": "phone",
+            "dn": "api-user.huami.com%2Capi-mifit.huami.com%2Capp-analytics.huami.com",
+            "grant_type": "access_token",
+            "lang": "zh_CN",
+            "os_version": "1.5.0",
+            "source": "com.xiaomi.hm.health",
+            "third_name": "email",
+        }
     r2 = requests.post(url2, data=data2, headers=headers).json()
     login_token = r2["token_info"]["login_token"]
     # print("login_token获取成功！")
@@ -191,7 +214,7 @@ def main(_user, _passwd, min_1, max_1):
     response = requests.post(url, data=data, headers=head).json()
     # print(response)
     result = f"[{now}]\n账号：{user[:3]}****{user[7:]}\n修改步数（{step}）[" + response['message'] + "]\n"
-    # print(result)
+    print(result)
     return result
 
 
