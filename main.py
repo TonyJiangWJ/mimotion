7759d6a1aca435cae8e7558474cf1163f2e3cc03
zhouqbb <zhouqbb@gmail.com>
2025-09-19 18:44:36 +0800

    Update main.py
    修改计算比例时的截止时间，超过时间默认比例1

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def get_min_max_by_time(hour=None, minute=None):
         hour = time_bj.hour
     if minute is None:
         minute = time_bj.minute
-    time_rate = min((hour * 60 + minute) / (22 * 60), 1)
+    time_rate = min((hour * 60 + minute) / (8 * 60), 1)
     min_step = get_int_value_default(config, 'MIN_STEP', 18000)
     max_step = get_int_value_default(config, 'MAX_STEP', 25000)
     return int(time_rate * min_step), int(time_rate * max_step)
